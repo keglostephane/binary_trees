b67fceb6723b0bffb67ef0ba6d27211f30c0ad34
Stephane Keglo <stephanekeglo@gmail.com>
2023-03-03 14:28:21 -0800

    fix 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *temp, *node = malloc(sizeof(binary_tree_t));
 
 	if (!node || !parent)
+	{
+		free(node);
 		return (NULL);
+	}
 
 	node->n = value;
 	node->parent = parent;
