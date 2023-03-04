8d2606e2482f601b648e77bcb99d38590caee00c
Stephane Keglo <stephanekeglo@gmail.com>
2023-03-03 18:25:17 -0800

    add 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ *
+ * @tree: pointer to the root node of the tree to delete
+ *
+ * if tree is NULL, do nothing
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *head = tree;
+
+	if (!head)
+		return ;
+
+	binary_tree_delete(head->left);
+	binary_tree_delete(head->right);
+
+	free(head);
+}
